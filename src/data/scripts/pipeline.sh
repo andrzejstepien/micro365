4f1e3228b16567be267e93be5e4314615e0d81e0
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-02 12:37:59 +0200

    rolled back 'form_of'

--- src/data/scripts/pipeline.sh
+++ src/data/scripts/pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ../processing
 
-jq -r '. | select((.pos=="noun") or (.pos=="verb") or (.pos=="adj") or (.pos=="adv")) | select((.word | test("[^a-z]"))|not) | {word: .word, form_of: .senses[].tags | any(. == "form-of"), type: .pos, pronunciation: .sounds[0].ipa, definitions: (try .senses|map(.glosses|join(" ")))}' \
+jq -r '. | select((.pos=="noun") or (.pos=="verb") or (.pos=="adj") or (.pos=="adv")) | select((.word | test("[^a-z]"))|not) | {word: .word, type: .pos, pronunciation: .sounds[0].ipa, definitions: (try .senses|map(.glosses|join(" ")))}' \
 wiktionary.json > wiktionary-p1.json
 
-jq --slurp '. | group_by(.word)[] | {word:.[0].word, pronunciation:.[0].pronunciation, meanings:[.[]|{type:.type, definitions:[try .definitions[]]| select(.!=[]) |map({(.):1})|add|keys_unsorted}]}' \
+jq --slurp '. | group_by(.word)[] | {word:.[0].word, pronunciation:.[0].pronunciation, meanings:[.[]|{type:.type, form_of:(try .form_of), definitions:[try .definitions[]]| select(.!=[]) |map({(.):1})|add|keys_unsorted}]}' \
 wiktionary-p1.json > wiktionary-p2.json
 
 jq --slurp '.' wiktionary-p2.json > wiktionary-p3.json
