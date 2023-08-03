d6865498bf6b16b78c0174ad3b2d647479f9bd3f
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-03 12:55:32 +0200

    installed knex

--- src/data/scripts/deleteMisspelledWords.cjs
+++ src/data/scripts/deleteMisspelledWords.cjs
@@ -3,13 +3,13 @@ const sqlite3 = require("sqlite3").verbose()
 const db = new sqlite3.Database("../database")
 
 db.serialize(() => {
-    db.each("SELECT * FROM prompts", [],
+    db.each("SELECT * FROM dictionary", [],
         function (err, row) {
             if (err) {
                 return console.error(err.message)
             }
             if (Spellchecker.isMisspelled(row.word)) {
-                db.run("DELETE from prompts WHERE word=?", [row.word], function (err) { if (err) { return console.error(err.message) } })
+                db.run("DELETE from dictionary WHERE word=?", [row.word], function (err) { if (err) { return console.error(err.message) } })
                 console.log(`deleted non-word ${row.word}`)
             }
         },
