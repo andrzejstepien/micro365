73277588bb35210e3a7974bb4b375bd9db5f91c1
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-10 20:36:32 +0200

    webhhooks working

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,12 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 const app = express()
 const port = 4000
-
-app.get('/api', (req,res) => {
-    console.log("webhook received")
+app.use(bodyParser.json())
+app.post('/api', (req,res) => {
+    console.log("webhook received:")
+    console.dir(req.body.body.note.text)
+    res.sendStatus(200)
 })
 
 app.listen(port, () => {
