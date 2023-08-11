ae795f3a902f5944c4ad37c22999e6e63e982ecc
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-11 16:30:47 +0200

    http logging

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import logger from "./logger.mjs";
+import pinoHTTP from 'pino-http'
 const app = express()
 const port = 4000
 app.use(bodyParser.json())
+app.use(
+    pinoHTTP({
+      logger,
+    })
+  )
 app.post('/api', (req,res) => {
     console.log("webhook received:")
     console.dir(req.body.body.note.text)
