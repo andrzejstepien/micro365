4dcc369fbf9b5eb03732bea5961c0732549686ce
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-08-12 18:42:05 +0200

    should run on startup

--- start.mjs
+++ start.mjs
@@ -4,6 +4,7 @@ import checkAndPublish from './checkAndPublish.mjs';
 
 export default function start(){
     logger.trace("start() called")
+    checkAndPublish()
     cron.schedule('* * * * *', () => {
         checkAndPublish()
         logger.trace('one-minute cron call');
